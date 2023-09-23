1c8dfb06ed3be2c3c87e9ada022f4c7b133c409c
VivekUppalapu <rrvivek16@gmail.com>
2023-09-22 21:28:46 -0400

    Changes

--- src/App.js
+++ src/App.js
@@ -6,9 +6,26 @@ function App() {
 
     return (
       <div className="App">
-        <div class="btn-wrapper">
- <a href="https://hax.psu.edu">
-   <button>Details</button> </a> </div>
+        <div class="wrap">
+  <div class="container">
+    <div class="header">
+      <h3> THE MCU </h3>
+    </div>
+    <img class="image" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapercave.com%2Fwp%2Fwp4884720.png&f=1&nofb=1&ipt=b86f47823ab6a8f5936b05270477294e1a27ee2b3b934a7789d434a72869fb8a&ipo=images">
+    </img>
+    <div class="descrip">
+      <p> Growing up, I was a huge MCU fan. I watched all of the movies, some more than others, and I think that the picture that sums up how I feel about it is this one, which is the final battle in Endgame. I just felt pure joy seeing all these characters together.</p>
+    </div>
+    <button class="Detail">
+      Details</button>
+  </div>
+</div>
+    <div class="allbtns">
+      <button class="duplicateBtn"> Duplicate Card</button>
+      <button class="changeBckgrnd"> Change Background </button>
+      <button class="ChangeTitle"> Change Title</button>
+      <button class="Delcrd"> Delete Card </button>
+    </div>
       </div>
     );
   }
